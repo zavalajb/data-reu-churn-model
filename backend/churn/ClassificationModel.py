596e949eeb5d029f3f2c7ce19e9137a9765ed98a
bzavalaj <bzavala@ciencias.unam.mx>
2024-06-24 14:48:29 -0600

    Se agrega la opcion de modelar usando GBTClassifier a la clase CalsssificationModel

--- backend/churn/ClassificationModel.py
+++ backend/churn/ClassificationModel.py
@@ -3,6 +3,7 @@
 from pyspark.sql.functions import lit
 # ML libraries
 from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
@@ -24,48 +25,61 @@ class ClassificationModel:
     """
     return self.data.randomSplit([0.8, 0.2])
 
-  def build_classification_model(self, labelCol: str, featuresCol: str, weightCol : str):
-    """
-      Ths method initialize a Random Forest Classifier, sets up parameter grid and creaes a CrossValidator
-
-         labelCol: name of the label column in the data frame, 
-         featuresCol: name of the features col in the data frame, 
-         weightCol : name of the weight col
-         return: a CrossValidator object that will be used to perfom k-fold coss-validation on a dataset
-    """
-
-    # Initialize the classification Model
-    randomforest = RandomForestClassifier(labelCol=labelCol, featuresCol=featuresCol,weightCol= weightCol,seed = 42)
-
-    # Create a ParamGridBuilder
-    paramGrid = (ParamGridBuilder()
-                .addGrid(randomforest.numTrees, [10, 20, 30])  # Number of trees
-                .addGrid(randomforest.maxDepth, [5, 10, 15])   # Maximum depth of the tree
-                .addGrid(randomforest.maxBins, [32, 64, 128])  # Number of bins used when discretizing continuous features
-                .addGrid(randomforest.featureSubsetStrategy, ['auto', 'sqrt', 'log2'])  # Strategy to select a subset of features
-                .build())    # Set evaluator
-    modelEvaluator = MulticlassClassificationEvaluator(labelCol=labelCol, predictionCol="prediction", metricName="weightedRecall")
-
-
-
-    # Create the CrossValidator
-    crossval = CrossValidator(estimator=randomforest,
-                          estimatorParamMaps=paramGrid,
-                          evaluator=modelEvaluator,
-                          numFolds=5)  
-
-    return crossval
-
-  def train_classification_model(self, labelCol: str, featuresCol: str, weightCol : str):
+  def build_classification_model(self, labelCol: str, featuresCol: str, weightCol : str, model: str):
     """
          Train the model with an specific param grid performing cross validation
 
          labelCol: name of the label column in the data frame, 
          featuresCol: name of the features col in the data frame, 
          weightCol : name of the weight col
-         return: the model that performed best during cross-validation in train dataset
-    """  
-    crossval = self.build_classification_model(labelCol=labelCol, featuresCol=featuresCol, weightCol=weightCol)
+         return: 
+    """
+
+    # Initialize the classification Model
+    if model == "RandomForestClassifier":
+      ClassificationModelInstance = RandomForestClassifier(labelCol=labelCol, featuresCol=featuresCol,weightCol= weightCol,seed = 42)
+
+
+      # Set considered parameter grid
+      # Create a ParamGridBuilder
+      paramGrid = (ParamGridBuilder()
+                .addGrid(ClassificationModelInstance.numTrees, [10, 20, 30])  # Number of trees
+                .addGrid(ClassificationModelInstance.maxDepth, [5, 10, 15])   # Maximum depth of the tree
+                .addGrid(ClassificationModelInstance.maxBins, [32, 64, 128])  # Number of bins used when discretizing continuous features
+                .addGrid(ClassificationModelInstance.featureSubsetStrategy, ['auto', 'sqrt', 'log2'])  # Strategy to select a subset of features
+                .build())    
+      
+    elif model == "GBTClassifier": 
+      # Initialize the classification Model
+      ClassificationModelInstance = GBTClassifier(labelCol=labelCol, featuresCol=featuresCol, seed = 42)
+      # Set considered parameter grid
+      # Create a ParamGridBuilder
+      paramGrid = (ParamGridBuilder()
+    .addGrid(ClassificationModelInstance.maxDepth, [5, 10, 15])
+    .addGrid(ClassificationModelInstance.maxBins, [32, 64,128])
+    #.addGrid(ClassificationModelInstance.minInstancesPerNode, [1, 2, 4])
+    #.addGrid(ClassificationModelInstance.stepSize, [0.1, 0.05, 0.01])
+    .addGrid(ClassificationModelInstance.subsamplingRate, [0.8, 1.0])
+    #.addGrid(ClassificationModelInstance.maxIter, [10, 20, 50])
+    .build())
+    else:
+      raise "El modelo no existe" 
+    # Set evaluator
+    modelEvaluator = MulticlassClassificationEvaluator(labelCol=labelCol, predictionCol="prediction", metricName="weightedRecall")
+
+
+
+    # Create the CrossValidator
+    crossval = CrossValidator(estimator=ClassificationModelInstance,
+                          estimatorParamMaps=paramGrid,
+                          evaluator=modelEvaluator,
+                          numFolds=5)  
+
+    return crossval
+
+  def train_classification_model(self, labelCol: str, featuresCol: str, weightCol : str, model : str):
+    
+    crossval = self.build_classification_model(labelCol=labelCol, featuresCol=featuresCol, weightCol=weightCol, model = model)
     cvModel = crossval.fit(self.train)
     return  cvModel.bestModel
 
