26420abff39ffa9f165573f2f210ee11a718e4d7
bzavalaj <bzavala@ciencias.unam.mx>
2024-06-26 17:19:01 -0600

    reversión de cambio por error en push

--- backend/churn/PreProcess.py
+++ backend/churn/PreProcess.py
@@ -1,15 +1,15 @@
 # Class for preproccessing the data
 
+import logging
 # SQL libraries
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame, Window
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, udf
+from pyspark.sql.functions import col, udf, when
 from pyspark.sql.types import IntegerType, FloatType, DoubleType, StringType, BooleanType, ArrayType, LongType, StructType, StructField
-from pyspark.ml.classification import GBTClassifier
 # PySpark ML libraries
 
 
-from pyspark.ml.feature import StringIndexer, IndexToString, OneHotEncoder, VectorAssembler
+from pyspark.ml.feature import StringIndexer, IndexToString, OneHotEncoder, VectorAssembler, BucketedRandomProjectionLSH
 
 class PreProcess:
 
@@ -54,6 +54,96 @@ class PreProcess:
       return columns_types_dict
   
 
+  def get_null_counts(self, df: DataFrame, with_percentages=False):
+    """
+    Counts the number of nulls of each column of a spark Dataframe and returns the counts in a separate Dataframe.
+    If with_percentages is set to True, also returns a Dataframe with the percentage of nulls in each column.
+
+    :param df: Spark DataFrame to process.
+    :param with_percentages: If set to True, the null percentages per column will also be calculated and returned
+    in an additional Dataframe
+
+    :return: Spark Dataframe with calculated null counts per column, or tuple of Spark Dataframes, where one Dataframe
+    will hold the column null counts and the other will hold the column null percentages.
+    """
+    null_counts = df.select([
+        F.sum(
+            F.when(F.col(column).isNull(), 1)\
+            .otherwise(0)
+        ).alias(column + "_nulls")
+        for column in df.columns
+    ])
+
+    if with_percentages:
+        total_count = df.count()
+        null_percentages = null_counts.select([
+            (F.col(column) / total_count).alias(column + "%")
+            for column in null_counts.columns
+        ])
+
+        return null_counts, null_percentages
+    
+    else:
+        return null_counts
+    
+
+  def clean_nulls(self, df: DataFrame, row_threshold: float, column_threshold: float, default_values: dict, unaccounted_nulls_behavior='drop_rows'):
+    """
+    Clean up null values in a Spark Dataframe according to a specified criteria.
+    This method can eliminate nulls by dropping columns, rows, or filling them up with a specific value.
+
+    :param df: Spark DataFrame to process.
+    :param row_threshold: Float value indicating the maximum proportion of null values that will be tolerated in a row. All rows with
+    a higher proportion of nulls will be dropped.
+    :param column_threshold: Float value indicating the maximum proportion of null values that will be tolerated in a coumn. All columns
+    with a higher proportion of nulls will be dropped.
+    :param default_values: Dictionary that specifies a mapping from column name (key) and desired fill value. The fill value can also be
+    a Spark aggregate function in order to fill null values with the corresponding calculation. Currently, the supported aggregate
+    functions are ('sum','count','avg','min', and 'max'). After dropping all rows and columns according to threshold rules, 
+    the remaining null values will be filled according to this mapping.
+    :param unnacounted_nulls_behavior: String, can be 'drop_rows' or 'drop_cols'. Specifies what action to take if null values are still
+    present in the Dataframe after all the previous rules have been applied. If 'drop_rows', all remaining rows with null values will
+    be dropped. If 'drop_cols', all remaining columns with null values will be dropped.
+
+    :return: Spark Dataframe with cleaned up null values.
+    """
+    SUPPORTED_AGGREGATE_FUNCTIONS = ('sum','count','avg','min','max')
+    
+    assert unaccounted_nulls_behavior in ('drop_rows', 'drop_cols'), "Parameter unnacounted_nulls behavior must be in ('drop_rows', 'drop_cols')"
+
+    total_count = df.count()
+
+    drop_cols = [
+        column for column in df.columns if (df.filter(F.col(column).isNull()).count() / total_count > column_threshold)
+    ]
+    df = df.drop(*drop_cols)
+
+    row_threshold_absolute = int(len(df.columns) * (1 - row_threshold))
+    df = df.dropna(thresh=row_threshold_absolute)
+
+    null_cols_updated = [
+        column for column in df.columns if (df.filter(F.col(column).isNull()).count() > 0)
+    ]
+    default_values_updated = {column: default_values[column] for column in null_cols_updated if column in default_values.keys()}
+    for column, fill_value in default_values_updated.items():
+        if fill_value in SUPPORTED_AGGREGATE_FUNCTIONS:
+            fill_value_agg = df.select(column).agg({column: fill_value}).first()[0]
+            df = df.fillna({column: fill_value_agg})
+        else:
+            df = df.fillna({column: fill_value})
+
+    null_cols_unnacounted = [
+        column for column in df.columns if (df.filter(F.col(column).isNull()).count() > 0)
+    ]
+    if null_cols_unnacounted:
+        if unaccounted_nulls_behavior == 'drop_rows':
+            df = df.dropna(how='any')
+        else:
+            df = df.drop(*null_cols_unnacounted)
+    
+    return df
+  
+
   def undersample_random(self, df: DataFrame, labels_column: str) -> DataFrame:
     """
     Performs random under-sampling of the majority class on imbalanced datasets. 
@@ -88,6 +178,53 @@ class PreProcess:
             union_df = union_df.union(processed_df)
     
     return union_df
+  
+
+  def undersample_nearmiss_v2(self, df: DataFrame, features_column: str, labels_column: str, k: int) -> DataFrame:
+    """
+    Performs under-sampling of the majority class on imbalanced datasets using the NEARMISS-2 algorithm. 
+    Currently, this implementation is only for binary classification problems. The majority class will be
+    under-sampled to have the same amount of data that the minority class
+
+    :param df: Spark DataFrame to process.
+    :param features_column: Name of the DataFrame column that holds the feature vectors
+    :param labels_column: Name of the DataFrame column that holds the label values
+    :param k: Number of neighbors to consider on the nearest neighbors-based heuristic
+    :return: Spark Dataframe with the majority class undersampled.
+    """
+    # Get data counts for each label and identify the minority class
+    label_counts = df.groupby(labels_column).count()
+
+    majority_count = label_counts.orderBy(F.desc(F.col('count'))).first()['count']
+
+    minority_label = label_counts.orderBy(col('count')).first()[labels_column]
+    minority_count = label_counts.orderBy(col('count')).first()['count']
+
+    # Assign unique ids to each minority and majority sample to facilitate calculations
+    df_minority = df.filter(col(labels_column) == minority_label).withColumn("id", F.monotonically_increasing_id())
+    df_majority = df.filter(col(labels_column) != minority_label).withColumn("id", F.monotonically_increasing_id())
+
+    # Fit a BucketRandomProjectionLSH model to calculate approximate euclidean distances
+    features_len = len(df.first()[features_column])
+    bucket_length = pow(majority_count, -1/features_len)
+
+    brp = BucketedRandomProjectionLSH(inputCol=features_column, outputCol="hashes", bucketLength=bucket_length, numHashTables=3)
+    model = brp.fit(df_majority)
+
+    df_distances = model.approxSimilarityJoin(df_majority, df_minority, float("inf"), distCol="distance")
+
+    # For each majority sample, only consider the k minority samples with the greatest distance
+    window_spec = Window.partitionBy("datasetA.id").orderBy(F.desc("distance"))
+    df_distances = df_distances.withColumn("distance_rank", F.row_number().over(window_spec))
+    df_distances = df_distances.filter(col("distance_rank") <= k)
+    # For each majority sample, compute the average distance to the k minority samples that were previously selected
+    df_distances = df_distances.groupBy("datasetA.id", "datasetA."+features_column, "datasetA."+labels_column).agg({"distance": "avg"})
+    df_distances = df_distances.withColumnRenamed("avg(distance)", "avg_distance")
+
+    # Select the majority samples with the smallest average distance
+    df_majority_undersampled = df_distances.orderBy("avg_distance").limit(minority_count).select(features_column, labels_column)
+
+    return df_minority.select(features_column, labels_column).union(df_majority_undersampled)
   
 
   def oversample_random(self, df: DataFrame, labels_column: str):
@@ -146,7 +283,7 @@ class PreProcess:
   def encoded_index_columns(self, df: DataFrame, columns_to_encode: list[tuple[str]]) -> DataFrame:
         
         """
-         Indexes the specified columns of a DataFrame.
+         Encode the specified columns of a DataFrame.
 
          :param df: Spark DataFrame to process.
          :param columns_to_encode: List of tuples with the names of the columns to be encoded and the names of the new encoded columns.
@@ -154,15 +291,15 @@ class PreProcess:
         """
 
         for col_name, new_col_name in columns_to_encode:
-            encoder =OneHotEncoder(inputCol=col_name, outputCol=new_col_name ,handleInvalid="keep").fit(df)
+            encoder =OneHotEncoder(inputCol=col_name, outputCol=new_col_name).fit(df)
             df = encoder.transform(df)
             # Store model StringIndexerModel in dict f or revert_string_index method
             self.encoded_models[new_col_name] = encoder
         return df
-  def vector_feature_column(self, df: DataFrame, feature_cols: list[str], outputFeatureCol : str) -> DataFrame:
+  def vector_feature_column(self, df: DataFrame, feature_cols: list[str]) -> DataFrame:
         
         """
-         Indexes the specified columns of a DataFrame.
+         Vectorize the specified columns of a DataFrame.
 
          :param df: Spark DataFrame to process.
          :param columns_to_create_feature_vector: List of names of the columns to be added to features vector.
@@ -170,7 +307,7 @@ class PreProcess:
         """
 
 # Create a VectorAssembler object
-        vector_assembler = VectorAssembler(inputCols=feature_cols, outputCol=outputFeatureCol, handleInvalid="skip")
+        vector_assembler = VectorAssembler(inputCols=feature_cols, outputCol="features", handleInvalid="skip")
 
         df = vector_assembler.transform(df)
             # Store model StringIndexerModel in dict f or revert_string_index method
@@ -293,6 +430,94 @@ class PreProcess:
 
       return df.withColumn("class_weights", get_class_weight_udf(col(labels_column)))
   
+  def churn(self, sdf, churn:str):
+      logger = logging.getLogger(__name__)
+      logging.basicConfig( encoding='utf-8', level=logging.INFO)
+      """
+      Identify the types of columns in a Spark DataFrame.
+
+      Args:
+      self: Reference to the class instance.
+      sdf (DataFrame): The input Spark DataFrame.
+      churn: The name of the churn column on the input Spark DataFrame.
+
+      Returns:
+      sdf: A Spark DataFrame with the churn column pre processed depending on its data type.
+      """
+      
+      # Identify columns by their data types
+      numeric_cols = [field.name for field in sdf.schema.fields if isinstance(field.dataType, (IntegerType, FloatType, DoubleType, LongType))]
+      categorical_cols = [field.name for field in sdf.schema.fields if isinstance(field.dataType, StringType)]
+      boolean_cols = [field.name for field in sdf.schema.fields if isinstance(field.dataType, BooleanType)]
+      array_cols = [field.name for field in sdf.schema.fields if isinstance(field.dataType, ArrayType)]
+      struct_cols = [field.name for field in sdf.schema.fields if isinstance(field.dataType, StructType)]
+      unknown_cols = [col for col in sdf.columns if col not in numeric_cols and col not in categorical_cols and col not in boolean_cols and col not in array_cols and col not in struct_cols]
+
+      # Create a dictionary to store column types
+      columns_types_dict = {
+          "numeric_cols": numeric_cols,
+          "categorical_cols": categorical_cols,
+          "boolean_cols": boolean_cols,
+          "array_cols": array_cols,
+          "struct_cols": struct_cols,
+          "unknown_cols": unknown_cols,
+      }
+      
+      churn1 = churn
+
+      found_churn1 = False
+
+      for key, value in columns_types_dict.items():
+        if isinstance(value, list):
+        # Iterate over list elements to check if column is present in list item
+          for item in value:
+            if isinstance(item, str) and item == churn1:
+                found_churn1 = True
+                # Column processing according to its data type
+                if key == "numeric_cols":
+                      indexer =StringIndexer(inputCol=churn1, outputCol="indexed_"+ churn1).fit(sdf)
+                      sdf = indexer.transform(sdf)
+                     # Store model StringIndexerModel in dict f or revert_string_index method
+                      self.indexer_models["indexed_"+ churn1] = indexer
+                     ## cambiar prints por login
+                      logger.info(f"Se encontró '{churn1}' en la lista de '{key}' , se aplicó string indexer")
+                elif key == "boolean_cols":  
+                    integer_column = when(col(churn1) == False, 0).otherwise(1)
+                    sdf = sdf.withColumn(churn1 + '_booleantointeger', integer_column) 
+                    logger.info(f"Se encontró '{churn1}' en la lista de '{key}' , se aplicó boolean to integer") 
+                elif key == "categorical_cols":
+                    stringIndexer = StringIndexer(inputCol= churn1, outputCol="indexed_"+ churn1)
+                    sdf = stringIndexer.fit(sdf).transform(sdf)
+                    sdf.show()
+                    logger.info(f"Se encontró '{churn1}' en la lista de '{key}', se aplicó string indexer")
+                break  
+          else:
+            continue  
+          break 
+      
+      if not found_churn1:
+       logger.info(f"'{churn1}' no es una columna del dataframe")
+   
+      return sdf
+
+  def churn_inverter(self, sdf, churn:str):
+      """
+      Inverts the churn values of  the churn column in a Spark DataFrame.
+
+      Args:
+      self: Reference to the class instance.
+      sdf (DataFrame): The input Spark DataFrame.
+      churn: The name of the churn column on the input Spark DataFrame.
+
+      Returns:
+      sdf: A Spark DataFrame with the churn column vlaues inverted.
+      """
+      churn1 = churn   
+      integer_column = when(col(churn1) == 0, 1).otherwise(0)
+      sdf = sdf.withColumn(churn1 + '_inverted', integer_column) 
+      sdf = sdf.drop(churn1)
+      return sdf
+  
   
   @classmethod
   def load_data(cls, file_path_df: str, spark):
@@ -310,7 +535,7 @@ class PreProcess:
     - ValueError: If the file format is not supported (i.e., not CSV, Parquet, or JSON).
     """
     if file_path_df.endswith('.csv'):
-        df = spark.read.option("header", True).csv(file_path_df)
+        df = spark.read.csv(file_path_df, header = True,inferSchema=True)
     elif file_path_df.endswith('.parquet'):
         df = spark.read.parquet(file_path_df)
     elif file_path_df.endswith('.json'):
